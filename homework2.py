1936442c7e2d842415469a0fb82a3da94c11e51a
Andru1812 <121110355+Andru1812@users.noreply.github.com>
2024-07-08 21:32:37 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kolvo_dz = 12
+kolvo_chsov = 1.5
+name_kurs = 'Python'
+time_dz = kolvo_chsov/kolvo_dz
+print('Курс:', name_kurs, ',', 'Всего задач:', kolvo_dz, ',', 'Затрачено часов:', kolvo_chsov, ',', 'Среднее время выполнения:', time_dz)
