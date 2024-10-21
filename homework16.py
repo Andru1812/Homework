6b231ba40877a2216136313707472d4913bc0ad5
Andru1812 <121110355+Andru1812@users.noreply.github.com>
2024-10-21 18:33:31 +0300

    Add files via upload

--- homework16.py
+++ homework16.py
@@ -1,3 +1,18 @@
+def summ_list(list_0):
+    if len(list_0) == 1:
+        if isinstance(list_0[0], int) or isinstance(list_0[0], float):
+            return list_0[0]
+        elif isinstance(list_0[0], str):
+            return len(list_0[0])
+        else:
+            return 0
+    else:
+        if isinstance(list_0[-1], int) or isinstance(list_0[-1], float):
+            return list_0[-1] + summ_list(list_0[:-1])
+        elif isinstance(list_0[-1], str):
+            return len(list_0[-1]) + summ_list(list_0[:-1])
+        else:
+            return summ_list(list_0[:-1])
 def calculate_structure_sum(list_0):
     if type(list_0) == tuple or type(list_0) == set:
         list_0 = [*list_0]
@@ -6,10 +21,10 @@ def calculate_structure_sum(list_0):
     i = 0
     k = len(list_0)
     while i != k:
-        b1 = type(list_0[i]) == list
-        b2 = type(list_0[i]) == tuple
-        b3 = type(list_0[i]) == set
-        b4 = type(list_0[i]) == dict
+        b1 = isinstance(list_0[i], list)
+        b2 = isinstance(list_0[i], tuple)
+        b3 = isinstance(list_0[i], set)
+        b4 = isinstance(list_0[i], dict)
         if b1 or b2 or b3:
             col = list_0.pop(i)
             list_0 = [*list_0, *col]
@@ -19,12 +34,7 @@ def calculate_structure_sum(list_0):
         else:
             i += 1
         k = len(list_0)
-    s = 0
-    for j in list_0:
-        if type(j) == int or type(j) == float:
-            s += j
-        elif type(j) == str:
-            s += len(j)
+    s = summ_list(list_0)
     return s
 
 data_structure = [
